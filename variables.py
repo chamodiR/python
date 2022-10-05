44fbb6b389481c754e9e5f8bcf3a2c85424440c9
chamodiR <115060140+chamodiR@users.noreply.github.com>
2022-10-05 20:14:02 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,25 @@
+x = 5
+y = "Chamo"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Rasa" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "Chamo"
+# is the same as
+x = 'Chamo'
+
+a = 4
+A = "Rasa"
+#A will not overwrite a
